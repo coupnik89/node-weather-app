dccd3ce72a47de73852e58f862c50b53c2b0847f
Tin <Tin@email>
2019-05-07 14:44:32 -0700

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-// fetch('https://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-        
-//     })
-// })
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    // json() = parsed json
-    response.json().then((data) => {
-        if(data.error) {
-            return console.log(data.error)
-        }
-
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const address = document.querySelector('#location')
@@ -26,7 +7,7 @@ const errorMsg = document.querySelector('#error')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+    fetch(`/weather?address=${search.value}`).then((response) => {
         // json() = parsed json
         response.json().then((data) => {
             if (data.error) {
